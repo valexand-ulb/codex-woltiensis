6e962610b93c802fd620d372d672a5f9abb5b88b
Vinovrski Alexandre <vinovrski.alexande@ulb.be>
2023-05-15 21:22:46 +0200

    - theme modification, trying to improve color palette.

---
+++ lib/model/colors.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor blueNeutral = MaterialColor(_blueNeutralPrimaryValue, <int, Color>{
+  50: Color(0xFFE6EEF7),
+  100: Color(0xFFC1D4EC),
+  200: Color(0xFF98B7DF),
+  300: Color(0xFF6E9AD2),
+  400: Color(0xFF4F84C9),
+  500: Color(_blueNeutralPrimaryValue),
+  600: Color(0xFF2B66B9),
+  700: Color(0xFF245BB1),
+  800: Color(0xFF1E51A9),
+  900: Color(0xFF133F9B),
+});
+const int _blueNeutralPrimaryValue = 0xFF306EBF;
+
+const MaterialColor blueNeutralAccent = MaterialColor(_blueNeutralAccentValue, <int, Color>{
+  100: Color(0xFFCEDCFF),
+  200: Color(_blueNeutralAccentValue),
+  400: Color(0xFF6893FF),
+  700: Color(0xFF4E80FF),
+});
+const int _blueNeutralAccentValue = 0xFF9BB7FF;
+
+const MaterialColor greenValid = MaterialColor(_greenValidPrimaryValue, <int, Color>{
+  50: Color(0xFFE9F7E6),
+  100: Color(0xFFC8ECC1),
+  200: Color(0xFFA4DF98),
+  300: Color(0xFF7FD26E),
+  400: Color(0xFF63C94F),
+  500: Color(_greenValidPrimaryValue),
+  600: Color(0xFF41B92B),
+  700: Color(0xFF38B124),
+  800: Color(0xFF30A91E),
+  900: Color(0xFF219B13),
+});
+const int _greenValidPrimaryValue = 0xFF48BF30;
+
+const MaterialColor greenValidAccent = MaterialColor(_greenValidAccentValue, <int, Color>{
+  100: Color(0xFFD2FFCE),
+  200: Color(_greenValidAccentValue),
+  400: Color(0xFF74FF68),
+  700: Color(0xFF5DFF4E),
+});
+const int _greenValidAccentValue = 0xFFA3FF9B;
+
+const MaterialColor orangeWarning = MaterialColor(_orangeWarningPrimaryValue, <int, Color>{
+  50: Color(0xFFFAF1E4),
+  100: Color(0xFFF4DDBC),
+  200: Color(0xFFECC690),
+  300: Color(0xFFE4AF64),
+  400: Color(0xFFDF9D42),
+  500: Color(_orangeWarningPrimaryValue),
+  600: Color(0xFFD5841D),
+  700: Color(0xFFCF7918),
+  800: Color(0xFFCA6F14),
+  900: Color(0xFFC05C0B),
+});
+const int _orangeWarningPrimaryValue = 0xFFD98C21;
+
+const MaterialColor orangeWarningAccent = MaterialColor(_orangeWarningAccentValue, <int, Color>{
+  100: Color(0xFFFFF4EC),
+  200: Color(_orangeWarningAccentValue),
+  400: Color(0xFFFFB786),
+  700: Color(0xFFFFA76D),
+});
+const int _orangeWarningAccentValue = 0xFFFFD5B9;
+
+const MaterialColor redAlert = MaterialColor(_redAlertPrimaryValue, <int, Color>{
+  50: Color(0xFFFAE8E4),
+  100: Color(0xFFF4C5BC),
+  200: Color(0xFFEC9F90),
+  300: Color(0xFFE47964),
+  400: Color(0xFFDF5C42),
+  500: Color(_redAlertPrimaryValue),
+  600: Color(0xFFD5391D),
+  700: Color(0xFFCF3118),
+  800: Color(0xFFCA2914),
+  900: Color(0xFFC01B0B),
+});
+const int _redAlertPrimaryValue = 0xFFD93F21;
+
+const MaterialColor redAlertAccent = MaterialColor(_redAlertAccentValue, <int, Color>{
+  100: Color(0xFFFFEDEC),
+  200: Color(_redAlertAccentValue),
+  400: Color(0xFFFF8E86),
+  700: Color(0xFFFF766D),
+});
+const int _redAlertAccentValue = 0xFFFFBEB9;
